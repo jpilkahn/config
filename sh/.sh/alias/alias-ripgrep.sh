63b416a6d1b6701db62107bbc5adb68a6481dccf
Johannes Pilkahn <j.pilkahn@gmail.com>
2020-12-28 04:20:55 +0100

    feat(ripgreprc): add javascript bundler types

---
+++ sh/.sh/alias/alias-ripgrep.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# alias-ripgrep.sh
+# ================
+
+alias rgs="rg --stats"
+alias rgc="rg --context 0"
+alias rgc5="rg --context 5"
--- sh/.sh/alias/alias.sh
+++ sh/.sh/alias/alias.sh
@@ -5,46 +5,15 @@
 #
 # Defines useful aliases.
 
-# ----------------------------------- posix ------------------------------------
-
-alias _ls="ls"
-alias ls="ls -T\${PIL_TABSIZE:-4} \$PIL_COLOR_FLAG"
-alias la="ls -alF"
-alias ll="ls -AlBFh"
-alias l="ls -F"
-
-# confirm before overwriting
-alias _cp="cp"
-alias _mv="mv"
-alias _rm="rm"
-alias cp="cp -i"
-alias mv="mv -i"
-alias rm="rm -i"
-
-alias _diff="diff"
-alias diff="diff -u"
-
-alias _free="free"
-alias free="free -m"
-
-alias more="less"
-
-alias grep="grep \$PIL_COLOR_FLAG"
-alias egrep="egrep \$PIL_COLOR_FLAG"
-alias fgrep="fgrep \$PIL_COLOR_FLAG"
-
-alias dmesg="fgrep \$PIL_COLOR_FLAG"
-
-# ----------------------------------- arch ------------------------------------
-
-alias pacman="pacman \$PIL_COLOR_FLAG"
-
-# ---------------------------------- custom -----------------------------------
-
-alias c="code"
-alias k="kdevelop"
-
-alias shell="konsole"
-alias term="terminator"
-alias term-proj="term -l proj"
-alias term-work="term -l work"
+# shellcheck source=./sh/.sh/alias/alias-arch.sh
+# shellcheck source=./sh/.sh/alias/alias-custom.sh
+# shellcheck source=./sh/.sh/alias/alias-posix.sh
+# shellcheck source=./sh/.sh/alias/alias-ripgrep.sh
+while read -r _include ; do
+    [ -r "$_include" ] && . "$_include"
+done <<- EOF
+	$HOME/.sh/alias/alias-arch.sh
+	$HOME/.sh/alias/alias-custom.sh
+	$HOME/.sh/alias/alias-posix.sh
+	$HOME/.sh/alias/alias-ripgrep.sh
+EOF
