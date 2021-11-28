893acc36d0480c1a0708f987cbf6e5d1788f8888
Johannes Pilkahn <j.pilkahn@gmail.com>
2021-11-28 11:00:29 +0100

    feat(sh): adjust helper aliases for yarn (sub)commands to yarn2

--- sh/.sh/alias/alias-custom.sh
+++ sh/.sh/alias/alias-custom.sh
@@ -53,11 +53,24 @@ alias svnr="svn rebase"
 alias svnd="svn dcommit"
 alias svnl="svn log --oneline --show-commit"
 
+__ywsat() {
+    _scope="$1"
+    _name="$2"
+    _cmd="$3"
+
+    yarn workspace "@$_scope/$_name" "$_cmd"
+}
+
 alias y="yarn"
 alias ycl="yarn clean"
 alias yi="yarn install"
 alias ycli="yarn clean:install"
-alias yw="yarn workspace"
-alias ywl="yarn workspaces list"
-alias ywlj="yarn workspaces list --json"
-alias yw@g="yarn workspace @goldeimer/"
+alias yup="yarn up"
+alias yupi="yarn upgrade-interactive"
+alias yws="yarn workspace"
+alias ywhy="yarn why"
+alias ywsl="yarn workspaces list"
+alias ywslj="yarn workspaces list --json"
+alias yws@=__ywsat
+alias yws@g="yws@= goldeimer"
+alias yy="yarn why"
