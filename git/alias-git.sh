6cd9ad921a1f39a20d5f9d5038115735de95b546
Johannes Pilkahn <j.pilkahn@gmail.com>
2023-04-16 03:30:12 +0200

    feat(git): add aliases for cloning, various restores, various resets, aborting a rebase, checking out ours or theirs, and the initial commit + sort 'alias' file

--- git/alias-git.sh
+++ git/alias-git.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 alias g="git"
+
 alias ga="g a"
 alias gaa="g aa"
 alias gai="g ai"
@@ -8,38 +9,69 @@ alias gass="g assume"
 alias gassa="g assume-all"
 alias gau="g au"
 alias gaurbc="g au && g rbc"
+
 alias gbr="g br"
 alias gbrc="g brc"
 alias gbrD="g brD"
 alias gbrm="g brm"
+
 alias gc="g c"
 alias gca="g ca"
-alias gcb="g cb"
 alias gcm="g cm"
+
+alias gcl="g cl"
+
 alias gcpi="g cpi"
 alias gcpia="g cpia"
 alias gcpic="g cpic"
+
 alias gco="g co"
+alias gcob="g cob"
+alias gcoo="g coo"
+alias gcot="g cot"
+
 alias gd="g d"
 alias gdc="g dc"
 alias gdcs="g dcs"
 alias gds="g ds"
+
 alias gfl="g fl"
+
 alias ggr="g gr"
+alias ggrf="g grf"
+
+alias gic="g ic"
+
 alias gl="g l"
 alias gll="g ll"
+
 alias gpl="g pl"
 alias gps="g ps"
 alias gpsf="g psf"
-alias gr="g r"
-alias gr1="g r1"
+
 alias grb="g rb"
+alias grba="g rba"
 alias grbc="g rbc"
 alias grbi="g rbi"
 alias grbi5="g rbi5"
 alias grbi10="g rbi10"
 alias grbm="g rbm"
+
+alias gr="g r"
+alias gr1="g r1"
+alias gr2="g r2"
 alias grh="g rh"
+alias grh1="g rh1"
+alias grh2="g rh2"
+alias grs="g rs"
+alias grs1="g rs1"
+alias grs2="g rs2"
+
+alias grst="g rst"
+alias grstg="g rstg"
+alias grsto="g rstg && g coo"
+alias grstt="g rstg && g cot"
+
 alias gst="g s"
 alias gss="g ss"
 alias gsh="g sh"
@@ -50,5 +82,6 @@ alias gstiu="g stiu"
 alias gstsh="g stsh"
 alias gstsh@="g stshat"
 alias gstshu@="g stshuat"
+
 alias guass="g unassume"
 alias guassa="g unassume-all"
