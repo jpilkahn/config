6bb02a0478f7b1cb9d7370cff6d9189b68c58938
Johannes Pilkahn <j.pilkahn@gmail.com>
2023-05-02 06:08:24 +0200

    chore(sh): improve nomenclature

--- sh/alias/alias.sh
+++ sh/alias/alias.sh
@@ -3,11 +3,9 @@
 _pwd="$HOME/.sh/alias"
 
 # shellcheck source=./rust/cargo/alias-cargo.sh
+# shellcheck source=./sh/alias/alias-coreutils.sh
 # shellcheck source=./cron/alias-cron.sh
 # shellcheck source=./diff/alias-diff.sh
-# shellcheck source=./sh/alias/alias-filesystem.sh
-# shellcheck source=./sh/alias/alias-sys-utils.sh
-# shellcheck source=./sh/alias/alias-terminal-emulator.sh
 # shellcheck source=./esp-idf/alias-esp.sh
 # shellcheck source=./git/alias-git.sh
 # shellcheck source=./grep/alias-grep.sh
@@ -15,16 +13,18 @@ _pwd="$HOME/.sh/alias"
 # shellcheck source=./pacman/alias-pacman.sh
 # shellcheck source=./rg/alias-rg.sh
 # shellcheck source=./svn/alias-svn.sh
+# shellcheck source=./sh/alias/alias-sys-utils.sh
+# shellcheck source=./sh/alias/alias-terminal-emulator.sh
 # shellcheck source=./vscode/alias-vscode.sh
 # shellcheck source=./yarn/alias-yarn.sh
 while read -r _include; do
     [ -r "$_include" ] && . "$_include"
 done <<-EOF
     $_pwd/alias-cargo.sh
+    $_pwd/alias-coreutils.sh
     $_pwd/alias-cron.sh
     $_pwd/alias-diff.sh
     $_pwd/alias-esp.sh
-    $_pwd/alias-filesystem.sh
     $_pwd/alias-git.sh
     $_pwd/alias-grep.sh
     $_pwd/alias-k8.sh
