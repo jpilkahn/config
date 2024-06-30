4a1c59ea957112b919185d3644241bfd2d7b322c
Johannes Pilkahn <j.pilkahn@gmail.com>
2024-07-01 00:27:55 +0200

    feat(esp): update esp-related script(s)

--- esp-idf/alias-esp.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-# shellcheck source=/opt/esp-idf/export.sh
-alias esp32env='. $IDF_PATH/export.sh'
--- esp-idf/env-esp.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-# espressif (esp32) sdk
-IDF_PATH="/opt/esp-idf" ; export IDF_PATH
-IDF_TOOLS_PATH="$HOME/.espressif" ; export IDF_TOOLS_PATH
